d6d9c33d65bd752a0a20bfe1e785d203ee00df25
Shamsul Arifin <arifinbd6@gmail.com>
2025-12-29 21:14:57 +0600

    Added window

--- Setup02/OpenGL/Main.cpp
+++ Setup02/OpenGL/Main.cpp
@@ -4,5 +4,37 @@
 
 int main()
 {
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	int windowHeight = 800;
+	int windowWidth = 800;
+
+	GLFWwindow* window = glfwCreateWindow(windowWidth, windowHeight, "Hello!", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window!" << std::endl;
+		glfwTerminate();
+
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	gladLoadGL();
+
+
+	glViewport(0, 0, windowWidth, windowHeight);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
 	return 0;
 }
