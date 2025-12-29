99a24462a8fb38233d6b35709df867038e6ac951
Shamsul Arifin <arifinbd6@gmail.com>
2025-12-29 22:27:32 +0600

    FIxed triangle

--- Setup02/OpenGL/Main.cpp
+++ Setup02/OpenGL/Main.cpp
@@ -2,23 +2,20 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-const char* vertextShaderSource =
-	"#version 330 core\n"
-	"layout (location = 0) in vec3 aPos;\n"
-	"void main()\n"
-	"{\n"
-	"	glPosition = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-	"}\0"
-;
-
-const char* fragmentShaderSource =
-	"#version 330 core\n"
-	"out vec4 FragColor;\n"
-	"void main()\n"
-	"{\n"
-	"	FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
-	"}\0"
-;
+// Vertex Shader source code
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+//Fragment Shader source code
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
+"}\n\0";
 
 int main()
 {
@@ -66,7 +63,7 @@ int main()
 	glViewport(0, 0, windowWidth, windowHeight);
 
 	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertexShader, 1, &vertextShaderSource, NULL);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
 	glCompileShader(vertexShader);
 
 	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
