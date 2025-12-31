c7d73ae7bd68df3257d0b5fbb21d610083b1e06f
Shamsul Arifin <arifinbd6@gmail.com>
2025-12-31 07:35:47 +0600

    Issue fixed, code commented

--- Setup02/OpenGL/Main.cpp
+++ Setup02/OpenGL/Main.cpp
@@ -7,109 +7,117 @@
 #include "VBO.h"
 #include "EBO.h"
 
+
+
 // Vertices coordinates
 GLfloat vertices[] =
 {
-	-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,	// Lower left corner
-	0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,		// Lower right corner
-	0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f,	// Upper corner
-	-0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f,	// Inner left
-	0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f,	// Inner right
-	0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f	// Inner down
+	-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower left corner
+	0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower right corner
+	0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f, // Upper corner
+	-0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner left
+	0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner right
+	0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f // Inner down
 };
 
+// Indices for vertices order
 GLuint indices[] =
 {
-	0, 3, 5,	// Lower left triangle
-	3, 2, 4,	// Lower right triangle
-	5, 4, 1		// Upper triangle
+	0, 3, 5, // Lower left triangle
+	3, 2, 4, // Lower right triangle
+	5, 4, 1 // Upper triangle
 };
+
+
 
 int main()
 {
-	// Init GLFW
+	// Initialize GLFW
 	glfwInit();
 
-	// Tell GLFW the version of OpenGL
+	// Tell GLFW what version of OpenGL we are using 
+	// In this case we are using OpenGL 3.3
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-
-	// Tell GLFW to use CORE profile
+	// Tell GLFW we are using the CORE profile
+	// So that means we only have the modern functions
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	// window hegiht and width
-	int windowHeight = 800;
 	int windowWidth = 800;
+	int windowheight = 800;
 
-	// create GLFWwindow object
-	GLFWwindow* window = glfwCreateWindow(windowWidth, windowHeight, "Hello!", NULL, NULL);
-
-	// error handling
+	// Create a GLFWwindow object of 800 by 800 pixels
+	GLFWwindow* window = glfwCreateWindow(windowWidth, windowheight, "Hello!", NULL, NULL);
+	// Error check if the window fails to create
 	if (window == NULL)
 	{
-		std::cout << "Failed to create GLFW window!" << std::endl;
+		std::cout << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
-
 		return -1;
 	}
-
-	// add window to current context
+	// Introduce the window into the current context
 	glfwMakeContextCurrent(window);
 
-	// load GLAD to configure OpenGL
+	//Load GLAD so it configures OpenGL
 	gladLoadGL();
+	// Specify the viewport of OpenGL in the Window
+	// In this case the viewport goes from x = 0, y = 0, to x = 800, y = 800
+	glViewport(0, 0, windowWidth, windowheight);
 
-	//Specify viewport to OpenGL
-	// from x = 0, y = 0, to x = windowWidth, y = windowHeight
-	glViewport(0, 0, windowWidth, windowHeight);
 
-	
-	// generates shader object
+
+	// Generates Shader object using shaders defualt.vert and default.frag
 	Shader shaderProgram("default.vert", "default.frag");
 
-	// Genrates vertex array object and binds it
+
+
+	// Generates Vertex Array Object and binds it
 	VAO VAO1;
 	VAO1.Bind();
 
-	// Generates VBO
+	// Generates Vertex Buffer Object and links it to vertices
 	VBO VBO1(vertices, sizeof(vertices));
-	// Generates EBO
+	// Generates Element Buffer Object and links it to indices
 	EBO EBO1(indices, sizeof(indices));
 
 	// Links VBO to VAO
 	VAO1.LinkVBO(VBO1, 0);
-
-	// Unbind all to prevent modifying
+	// Unbind all to prevent accidentally modifying them
 	VAO1.Unbind();
 	VBO1.Unbind();
 	EBO1.Unbind();
 
-	// main while loop
+
+
+	// Main while loop
 	while (!glfwWindowShouldClose(window))
 	{
-		// Specify color of the background
+		// Specify the color of the background
 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-		// Clean the back buffer and assign the new color
+		// Clean the back buffer and assign the new color to it
 		glClear(GL_COLOR_BUFFER_BIT);
-		// Specify OpenGL which shader program to run
+		// Tell OpenGL which Shader Program we want to use
 		shaderProgram.Activate();
+		// Bind the VAO so OpenGL knows to use it
 		VAO1.Bind();
+		// Draw primitives, number of indices, datatype of indices, index of indices
 		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
 		// Swap the back buffer with the front buffer
 		glfwSwapBuffers(window);
-
-		// takes care of all GLFW events
+		// Take care of all GLFW events
 		glfwPollEvents();
 	}
 
-	// Delete all
+
+
+	// Delete all the objects we've created
 	VAO1.Delete();
 	VBO1.Delete();
 	EBO1.Delete();
 	shaderProgram.Delete();
-
-	// delete window before closing
+	// Delete window before ending the program
 	glfwDestroyWindow(window);
+	// Terminate GLFW before ending the program
 	glfwTerminate();
 	return 0;
 }
--- Setup02/OpenGL/VAO.h
+++ Setup02/OpenGL/VAO.h
@@ -2,20 +2,23 @@
 #define VAO_CLASS_H
 
 #include <glad/glad.h>
-#include"VBO.h"
+#include "VBO.h"
 
 class VAO
 {
 public:
+	// ID reference for the Vertex Array Object
 	GLuint ID;
+	// Constructor that generates a VAO ID
 	VAO();
 
-	void LinkVBO(VBO VBO, GLuint layout);
-
+	// Links a VBO to the VAO using a certain layout
+	void LinkVBO(VBO& VBO, GLuint layout);
+	// Binds the VAO
 	void Bind();
+	// Unbinds the VAO
 	void Unbind();
+	// Deletes the VAO
 	void Delete();
 };
-
 #endif
-
