8b5110b960979134946c67773daf5543ceca6f39
Shamsul Arifin <arifinbd6@gmail.com>
2026-01-06 09:35:07 +0600

    Added camera cpp

--- Setup02/OpenGL/Camera.h
+++ Setup02/OpenGL/Camera.h
@@ -14,7 +14,7 @@
 class Camera
 {
 public:
-	glm::vec3 position;
+	glm::vec3 Position;
 	glm::vec3 Orientation = glm::vec3(0.0f, 0.0f, -1.0f);
 	glm::vec3 Up = glm::vec3(0.0f, 1.0f, 0.0f);
 
@@ -23,6 +23,8 @@ public:
 
 	float speed = 0.1f;
 	float sensitivity = 100.0f;
+
+	Camera(int width, int height, glm::vec3 position);
 
 	void Matrix(float FOVdeg, float nearPlane, float farPlane, Shader& shader, const char* uniform);
 	void Inputs(GLFWwindow* window);
