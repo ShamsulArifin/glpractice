62637ff3005b2891e32ddd0a57753a0bb8ffcdad
Shamsul Arifin <arifinbd6@gmail.com>
2026-01-06 08:54:55 +0600

    Fixed pyramid texture

--- Setup02/OpenGL/Main.cpp
+++ Setup02/OpenGL/Main.cpp
@@ -103,7 +103,7 @@ int main()
 
 
 	// Texture
-	Texture popCat("brick.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
+	Texture popCat("meme_cat.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
 	popCat.texUnit(shaderProgram, "tex0", 0);
 
 	float rotation = 0.0f;
